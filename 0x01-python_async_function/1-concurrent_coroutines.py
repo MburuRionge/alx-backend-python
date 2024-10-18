5faf476da51015777b62bcf6c2086690eedaeb7b
MburuRionge <victormburu3@gmail.com>
2024-10-18 12:41:05 +0000

    How to run concurrent coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a coroutine that returns the list of all delays"""
+import asyncio
+import random
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Return a list of ascending delays"""
+    coroutine = []
+    delays = []
+
+    for _ in range(n):
+        coroutine.append(wait_random(max_delay))
+
+    for cors in asyncio.as_completed(coroutine):
+        delays.append(await cors)
+
+    return delays
---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a function that returns a asyncio.Task"""
+from asyncio import Task, create_task
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Task:
+    """Returns asyncio.Task"""
+    return create_task(wait_random(max_delay))
+
